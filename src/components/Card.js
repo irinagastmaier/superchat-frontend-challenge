de43ef32773623eb5fb1e973c1020cecec469da5
Irina <irinagmarques@hotmail.com>
2021-11-04 22:16:29 +0100

    logic for retrive params from url

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 
 export default function Card() {
-	const { params } = useParams();
-	console.log(params)
+	const { values } = useParams();
+	console.log(values);
 	//`http://localhost:3000/${user}/${repo}/${icon}/${color}`
 	return (
 		<div>
 			<h1>card</h1>
-			<h1>{params}</h1>
+			<h1>{values}</h1>
 		</div>
 	);
 }
