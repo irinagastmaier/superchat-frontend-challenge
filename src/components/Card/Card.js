9eef86871d32704ae00ad92c117dc0e99dd5f6d5
Irina <irinagmarques@hotmail.com>
2021-11-07 18:17:00 +0100

    minor changes after deployment

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { fetchContributors, fetchRepo, updateStarRepo } from '../../actions/actions';
@@ -11,6 +11,10 @@ import { RiGithubLine } from 'react-icons/ri';
 import styles from './Card.module.scss';
 
 export default function Card() {
+	const [message, setMessage] = useState({
+		submitting: false,
+		status: null,
+	});
 	const { values } = useParams();
 	const array = values.split(',');
 	const color = '#' + array[2];
@@ -43,11 +47,25 @@ export default function Card() {
 	} else if (icon === 'github2') {
 		icon = <FaGithubAlt className={styles.icon} value="github2" />;
 	} else {
-		<RiGithubLine className={styles.icon} value="github3" />;
+		icon = <RiGithubLine className={styles.icon} value="github3" />;
 	}
+
+	let timer;
+	const handleMessage = (ok, msg) => {
+		setMessage({
+			submitting: false,
+			status: { ok, msg },
+		});
+		timer = setTimeout(() => {
+			window.location.reload();
+		}, 5000);
+		return () => clearTimeout(timer);
+	};
 
 	const handleUpdate = () => {
 		dispatch(updateStarRepo(user, repo, { data: user }));
+		setMessage({ submitting: true });
+		handleMessage(true, 'Project starred!');
 	};
 
 	return (
@@ -78,6 +96,11 @@ export default function Card() {
 				<button type="submit" onClick={handleUpdate} className="btn">
 					<AiFillStar /> Repository
 				</button>
+				{message.status && (
+					<p className={!message.status.ok ? 'errorMsg' : ''} style={{ fontSize: '0.75rem', color: 'white' }}>
+						{message.status.msg}
+					</p>
+				)}
 			</div>
 		</div>
 	);
